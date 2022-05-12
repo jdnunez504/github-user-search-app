6ce7d77b6d08971e32801835143711703801b32d
Joaquin Daniel Nunez <jdnunez@zoho.com>
2022-05-12 15:36:03 -0500

    Started API scripting

--- local/js/main.js
+++ local/js/main.js
@@ -1,3 +1,4 @@
+const body = document.getElementsByTagName('body')[0];
 const uiModeToggle = document.getElementById('ui-mode-toggle');
 const modeName = document.querySelector('.mode-name');
 const modeNameAlt = document.querySelector('.mode-name-alt');
@@ -5,18 +6,31 @@ const modeSvg = document.querySelector('.mode-svg');
 const modeSvgAlt = document.querySelector('.mode-svg-alt');
 const modeIcon = document.querySelector('.mode-icon');
 const modeIconAlt = document.querySelector('.mode-icon-alt');
-const body = document.getElementsByTagName('body')[0];
 const appTitle = document.querySelector('.app-title');
 const searchContainer = document.querySelector('.search-container');
+const searchButton = document.getElementById('search-button');
 const userProfileCard = document.querySelector('.user-profile-card');
-const userName = document.querySelector('.user-name');
+const avatarDesktop = document.getElementById('avatar--desktop');
+const avatar = document.getElementById('avatar');
+const userName = document.getElementById('user-name');
+const userLogin = document.getElementById('user-login');
 const joinDate = document.querySelector('.join-date');
-const userDescription = document.querySelector('.user-description');
+const userBio = document.querySelector('.user-bio');
 const statsContainer = document.querySelector('.stats-container');
 const statsHeadingNodeList = document.querySelectorAll('.stats-heading');
 const statsDataNodeList = document.querySelectorAll('.stats-data');
+const publicRepos = document.getElementById('public-repos');
+const followers = document.getElementById('followers');
+const following = document.getElementById('following');
 const aboutDataNodeList = document.querySelectorAll('.about-data');
-const blogLink = document.querySelector('.blog-link');
+const _location = document.getElementById('location');
+const _locationIcon = document.getElementById('location-icon');
+const blog = document.getElementById('blog');
+const blogIcon = document.getElementById('blog-icon');
+const twitterUsername = document.getElementById('twitter-username');
+const twitterIcon = document.getElementById('twitter-icon');
+const company = document.getElementById('company');
+const companyIcon = document.getElementById('company-icon');
 const aboutIconNodeList = document.querySelectorAll('.about-icon');
 const searchInput = document.querySelector('.search-input');
 
@@ -35,12 +49,11 @@ uiModeToggle.addEventListener('click', function(e) {
 	userProfileCard.classList.toggle('alt-mode');
 	userName.classList.toggle('alt-mode');
 	joinDate.classList.toggle('alt-mode');
-	userDescription.classList.toggle('alt-mode');
+	userBio.classList.toggle('alt-mode');
 	statsContainer.classList.toggle('alt-mode'); 
 	statsHeadingNodeList.forEach(toggleAltMode);
 	statsDataNodeList.forEach(toggleAltMode);
 	aboutDataNodeList.forEach(toggleAltMode);
-	//blogLink.classList.toggle('alt-mode');
 	aboutIconNodeList.forEach(toggleAltMode);
 	searchInput.classList.toggle('alt-mode');
 
@@ -51,6 +64,10 @@ function toggleAltMode(val, i, arr) {
 	val.classList.toggle('alt-mode');
 }
 
+function setUnavailable(val, i, arr) {
+	val.classList.add('unavailable');
+}
+
 function requestUserData(username) {
 	const xhr = new XMLHttpRequest();
 	const url = `https://api.github.com/users/${username}`;
@@ -58,19 +75,47 @@ function requestUserData(username) {
 	xhr.onload = function() {
 		const data = JSON.parse(this.response);
 		console.log(data);
-		console.log(data.login);
-		console.log(data.avatar_url);
-		console.log(data.html_url);
-		console.log(data.name);
-		console.log(data.blog);
-		console.log(data.location);
-		console.log(data.company);
-		console.log(data.bio);
-		console.log(data.twitter_username);
-		console.log(data.public_repos);
-		console.log(data.followers);
-		console.log(data.following);
+		avatar.setAttribute("src", `${data.avatar_url}`);
+		avatarDesktop.setAttribute("src", `${data.avatar_url}`);
+		userName.textContent = `${data.name}`;
+		userLogin.textContent = `@${data.login}`;
 		console.log(data.created_at);
+		userBio.textContent = `${data.bio}`;
+		publicRepos.textContent = `${data.public_repos}`;
+		followers.textContent = `${data.followers}`;
+		following.textContent = `${data.following}`;
+
+		if(data.location == null) {
+			setUnavailable(_location);
+			setUnavailable(_locationIcon);
+			_location.textContent = 'Not Available';
+		}
+		else
+			_location.textContent = `${data.location}`;
+
+		if(data.blog == null) {
+			setUnavailable(blog);
+			setUnavailable(blogIcon);
+			blog.textContent = 'Not Available';
+		}
+		else
+			blog.textContent = `${data.blog}`;
+
+		if(data.twitter_username == null) {
+			setUnavailable(twitterUsername);
+			setUnavailable(twitterIcon);
+			twitterUsername.textContent = 'Not Available'; 
+		}
+		else
+			twitterUsername.textContent = `#${data.twitter_username}`;
+			
+		if(data.company == null) {
+			setUnavailable(company);
+			setUnavailable(companyIcon);
+			company.textContent = 'Not Available';
+		}
+		else
+			company.textContent = `@${data.company}`;
 	}
 	xhr.send();
 }
